f2b0e702d16ac147c1861e6a79d4adf5b1ed6b36
Mark Ruch <mark.ruch19@gmail.com>
2018-12-03 19:33:41 -0600

    Initial commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Mark Ruch on 11/28/18.
+//  Copyright © 2018 Mark Ruch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
